c967df1d1cb61cba5d59011631bd0e057f32f028
Michael <171223687+Bazorko@users.noreply.github.com>
2024-06-07 22:27:06 -0400

    Added some responsiveness to the page.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,14 +3,14 @@ const Home = () => {
     return(<>
         <main className="h-screen bg-blue-500">
             <Container>
-                <section className="h-screen flex flex-col gap-y-6 items-center">
-                    <section className="mt-48">
+                <section className="h-screen flex flex-col gap-y-6 justify-center items-center">
+                    <section>
                         <h1 className="text-slate-50 text-5xl">Weather</h1>
                     </section>
-                    <section className="w-9/12">
-                        <form action="" className="flex flex-col gap-y-4">
-                            <input type="text" placeholder="Enter a city" name="city" className="py-1 pl-1 border-2 rounded-lg border-slate-50"/>
-                            <button className="p-1 border-2 border-slate-50 text-slate-50 rounded-lg" type="submit">Search</button>
+                    <section className="w-full sm:w-10/12">
+                        <form action="" className="flex flex-col md:flex-row gap-y-4 md:gap-x-4 md:justify-center">
+                            <input type="text" placeholder="Enter a city" name="city" className="py-2 pl-1 border-2 rounded-lg border-slate-50 w-full md:w-96"/>
+                            <button className="md:w-40 p-1 py-2 border-2 border-slate-50 text-slate-50 rounded-lg" type="submit">Search</button>
                         </form>
                     </section>
                     <section>
