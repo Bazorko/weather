ff205106becb2b5fb1d215062617fe995e87ad87
Michael <171223687+Bazorko@users.noreply.github.com>
2024-06-22 13:40:35 -0400

    Added some basic form error checking functionality.

--- frontend/src/components/account/SignUp.jsx
+++ frontend/src/components/account/SignUp.jsx
@@ -4,6 +4,11 @@ const SignUp = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [verifyPassword, setVerifyPassword] = useState("");
+    const [error, setHandleError] = useState("");
+    const handleError = (errorMessage) => {
+        setHandleError("");
+        setHandleError(errorMessage);
+    }
     const handleUsernameInput = (event) => {
         setUsername(event.target.value);
     }
@@ -19,26 +24,38 @@ const SignUp = (props) => {
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         console.log(`Account: ${username}, ${email}, ${password}`);
-        const response = await fetch("http://localhost:3000/user/signup", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            credentials: "include",
-            body: JSON.stringify({username, email, password}),
-        })
-        //You could for example "setData" with the json that the server sent back
-        const json = await response.json();
+        if (password !== verifyPassword){
+            handleError("Your passwords do not match.");
+        }
+        else if (password.length < 8 && verifyPassword.length < 8){
+            handleError("Your password must be longer than 8 characters.");
+        }
+        else if (username < 3){
+            handleError("Your username must be longer than 3 characters.");
+        }
+        else if(password === verifyPassword){
+            const response = await fetch("http://localhost:3000/user/signup", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+                body: JSON.stringify({username, email, password}),
+            })
+            //You could for example "setData" with the json that the server sent back
+            const json = await response.json();
+        }
     }
     return(<>
         <section>
             <p className="text-3xl">Sign Up.</p>
         </section>
         <section className="pt-6 w-full">
+            <p>{error ?? error}</p>
             <form onSubmit={handleFormSubmit} className="flex flex-col w-full gap-2">
-                <input type="text" value={username} onChange={handleUsernameInput} className="py-1.5 pl-1.5 border-zinc-600 border-2 rounded-lg" placeholder="Enter a username"/>
-                <input type="email" value={email} onChange={handleEmailInput} className="py-1.5 pl-1.5 border-zinc-600 border-2 rounded-lg" placeholder="Enter your email address"/>
+                <input type="text" required value={username} onChange={handleUsernameInput} className="py-1.5 pl-1.5 border-zinc-600 border-2 rounded-lg" placeholder="Enter a username"/>
+                <input type="email" required value={email} onChange={handleEmailInput} className="py-1.5 pl-1.5 border-zinc-600 border-2 rounded-lg" placeholder="Enter your email address"/>
                 <div className="flex flex-col md:flex-row gap-2">
-                    <input type="password" value={password} onChange={handlePasswordInput} className="py-1.5 pl-1.5 border-zinc-600 border-2 rounded-lg w-full md:w-3/6 text-sm" placeholder="Enter a password."/>
-                    <input type="password" value={verifyPassword} onChange={handleVerifyPasswordInput} className="text-sm py-1.5 pl-1.5 border-zinc-600 border-2 rounded-lg w-full md:w-3/6" placeholder="Re-enter your password"/>
+                    <input type="password" required value={password} onChange={handlePasswordInput} className="py-1.5 pl-1.5 border-zinc-600 border-2 rounded-lg w-full md:w-3/6 text-sm" placeholder="Enter a password."/>
+                    <input type="password" required value={verifyPassword} onChange={handleVerifyPasswordInput} className="text-sm py-1.5 pl-1.5 border-zinc-600 border-2 rounded-lg w-full md:w-3/6" placeholder="Re-enter your password"/>
                 </div>
                 <button type="submit" className="py-2 border-2 rounded-lg bg-blue-500 text-slate-50">Sign Up</button>
             </form>
