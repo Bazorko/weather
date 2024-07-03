da79db6df63dc252d22266fce79a69d86b61eb26
Michael <171223687+Bazorko@users.noreply.github.com>
2024-07-02 21:12:13 -0400

    Template styled to be more responsive.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -28,6 +28,8 @@ const Dashboard = () => {
     }
     if(user){
         const { username, email } = user;
+        const usernameCapitalized = username[0].toUpperCase()+username.slice(1).toLowerCase();
+        const emailLowercase = email.toLowerCase();
         return(<>
             <section className="h-screen bg-slate-50" onClick={closeAccountModal}>
                 <nav className="w-full h-fit bg-blue-500 p-5 px-8 lg:px-32 flex flex-row items-center relative">
@@ -38,25 +40,26 @@ const Dashboard = () => {
                     </section>
                     <section className="w-2/4 flex flex-col">
                         <div className="ml-auto">
-                            {isAccountModalOpen ? <AccountModal username={username} email={email} /> : <div className="w-12 h-12 bg-slate-50 rounded-full cursor-pointer" onClick={openAccountModal}></div>}
+                            {isAccountModalOpen ? <AccountModal username={usernameCapitalized} email={emailLowercase} /> : <div className="w-12 h-12 bg-slate-50 rounded-full cursor-pointer" onClick={openAccountModal}></div>}
                         </div>
                     </section>
                 </nav>
                 <Container>
-                    <section>
+                    <section className="flex flex-col items-center">
+                        <h1>{usernameCapitalized}'s Saved Cities</h1>
                         {cities.map(city => {
                             return (
                                 <>
-                                    <section className="flex rounded-lg border-2 border-gray-900">
-                                        <section>
-                                            <h2>{city.city}</h2>
-                                            <h3>{city.state}</h3>
+                                    <section key={city.key} className="flex w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 m-4 justify-center rounded-lg border-2 border-neutral-400 p-4">
+                                        <section className="flex-1 text-center">
+                                            <h2 className="p-2">{city.city}</h2>
+                                            <h3 className="p-2">{city.state}</h3>
                                         </section>
-                                        <section>
-                                            <p>{city.temp}</p>
+                                        <section className="flex-1 text-center">
+                                            <p className="p-2">{city.temp}&deg; F</p>
                                             <section className="flex">
-                                                <p>{city.temp_min}</p>
-                                                <p>{city.temp_max}</p>
+                                                <p className="flex-1 text-right p-2">{city.temp_min}&deg; F</p>
+                                                <p className="flex-1 text-left p-2">{city.temp_max}&deg; F</p>
                                             </section>
                                         </section>
                                     </section>
